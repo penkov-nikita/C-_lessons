7a4c3f1cba541e80ef56ef369de8dacfb9c39e4a
Penkov.Nikita <penkov.nikita@gmail.com>
2024-02-14 15:20:19 +0300

    add some variables

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+int value = 100; 
+Console.WriteLine(value);
+
+double d = 3.33;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
