b86bc0497d946f1237f1ccd8a91050dccdffd12b
Penkov.Nikita <penkov.nikita@gmail.com>
2024-02-14 16:05:25 +0300

    add les1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 3;
+int b = 5;
+int c = 8;
+int d = 1;
+int max = a; 
+
+if (b > max){
+    max = b; 
+}
+if (c > max){
+    max = c; 
+}
+if (d > max){
+    max = d; 
+}
+Console.WriteLine(max);
